c79c5a06f85c30d2806869c9e41d4672a1d8245c
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2019-09-09 13:36:29 +0200

    start python interpreter with pre-loaded modules

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: shell
+
+shell:
+	python -i shell.py
---
+++ shell.py
@@ -0,0 +1,5 @@
+from bunch import Bunch
+from douglaspeucker import *
+
+start = Bunch(lat=46.94809, lon=7.44744)
+end = Bunch(lat=47.36667, lon=8.55)
